17b1ffb116029d7a70950cbbe8d83d2b973ea382
Rauljp16 <raul_astra16@hotmail.com>
2025-02-21 15:43:49 +0100

    feat: componente slider creado y en construccion

--- components/Initial_Info.tsx
+++ components/Initial_Info.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Initial_Info() {
   return (
-    <section className="relative flex flex-col w-full h-[calc(100vh-16vh)]">
+    <section className="relative flex flex-col w-full h-[calc(100vh-20vh)]">
       <div className="absolute -z-10 -top-32 right-0 w-1/4 h-2/4 bg-contrast blur-[1px] border "></div>
       <div className="flex justify-center w-full h-[65%]">
         <article className="relative top-16 flex flex-col w-[13%] max-w-12 h-1/2 gap-2">
@@ -41,7 +41,7 @@ function Initial_Info() {
       </div>
       <div className="flex flex-col justify-between w-full h-[35%]  px-4 py-4 ">
         <h1 className="text-4xl font-thin">Un coche para tí</h1>
-        <p className="text-xs w-3/4">
+        <p className="text-xs w-full max-w-80">
           Encuentra el coche perfecto para ti, con opciones que se adaptan a tu
           estilo y presupuesto.
         </p>
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,70 +1,58 @@
 "use client";
 
-import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import type { Swiper as SwiperType } from "swiper";
+import Image from "next/image";
+import { Car } from "@/app/page";
 
-// Importa los estilos de Swiper
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import { Car } from "@/app/page";
-import Image from "next/image";
-
-// Estilos personalizados
 const customStyles = `
   .swiper-slide {
-    transition: all 0.5
+    transform: scaleY(0.8);
+    z-index: 0;
   }
   .swiper-slide-active {
-    z-index: 2;
+  transform: scaleY(1);
+      z-index: 10;
   }
 `;
 
 function Slider({ cars }: { cars: Car[] }) {
-  const [swiper, setSwiper] = useState<SwiperType | null>(null);
-
   const sortedCars = cars.sort(
     (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
   );
   const latestCars = sortedCars.slice(0, 5);
 
-  // Configuración del slider
   const swiperParams = {
     modules: [Navigation, Pagination, Autoplay],
-    spaceBetween: 10,
-    slidesPerView: 3,
+    slidesPerView: 1.2,
+    spaceBetween: 20,
     centeredSlides: true,
     loop: true,
-    autoplay: { delay: 5000, disableOnInteraction: false },
-  };
-
-  // Funciones de navegación personalizadas
-  const goNext = () => {
-    if (swiper) swiper.slideNext();
-  };
-
-  const goPrev = () => {
-    if (swiper) swiper.slidePrev();
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
   };
 
   return (
-    <div className="relative w-full h-60 py-4 mb-20">
+    <div className="relative w-full h-[300px] py-4 mb-20">
       <h1 className="text-2xl p-2 pt-0">¡Novedades!</h1>
       <style>{customStyles}</style>
-      <Swiper {...swiperParams} onSwiper={setSwiper} className="h-full">
+      <Swiper {...swiperParams} className="w-full h-full">
         {latestCars.map((car, index) => (
-          <SwiperSlide key={index} className="relative">
-            <div className="relative h-full w-full">
+          <SwiperSlide key={index} style={{ width: "auto" }}>
+            <div className="relative h-full w-[280px]">
               <Image
                 src={car.foto}
                 layout="fill"
                 objectFit="cover"
-                alt={`Car ${index}`}
+                alt={`${car.marca} ${car.modelo}`}
               />
-              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2">
+              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 rounded-b-lg">
                 <h3 className="text-sm font-bold">{car.marca}</h3>
                 <p className="text-xs">{car.modelo}</p>
               </div>
@@ -72,30 +60,6 @@ function Slider({ cars }: { cars: Car[] }) {
           </SwiperSlide>
         ))}
       </Swiper>
-      <button
-        onClick={goPrev}
-        className="absolute left-[40%] -bottom-16 z-10 p-[2px] bg-neutral-700/40 rounded-lg hover:bg-white "
-      >
-        <Image
-          src="/svg/prev.svg"
-          width={24}
-          height={24}
-          alt="Botón prev"
-          className="w-6 h-6"
-        />
-      </button>
-      <button
-        onClick={goNext}
-        className="absolute right-[40%] -bottom-16 z-10 p-[2px] bg-neutral-700/40 rounded-lg hover:bg-white "
-      >
-        <Image
-          src="/svg/next.svg"
-          width={24}
-          height={24}
-          alt="Botón next"
-          className="w-6 h-6"
-        />
-      </button>
     </div>
   );
 }
