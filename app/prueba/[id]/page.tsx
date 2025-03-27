99bd125d056706aa3e410d03408d9bd01bca0ae4
Rauljp16 <raul_astra16@hotmail.com>
2025-03-27 21:47:47 +0100

    componente de prueba

--- app/prueba/[id]/page.tsx
+++ app/prueba/[id]/page.tsx
@@ -7,10 +7,12 @@ function Page() {
   return (
     <div className="pt-40">
       <p>pagina de prueba</p>
+      <p>pagina de test</p>
       <p>Coche ID: {params.id}</p>
     </div>
   );
 }
 //mirar por que no funciona el fetch de la api de coches.
+//Ver de que va esto
 
 export default Page;
