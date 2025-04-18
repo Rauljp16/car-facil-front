b98e8cf6df398499d4c1d418550c0d425af01b7b
Rauljp16 <raul_astra16@hotmail.com>
2025-04-18 18:35:19 +0200

    pagina construcción

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         <Header />
         <Construccion />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
