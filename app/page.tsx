469b5854f2d3e31f7444748ae2886dfbcae97ea3
Rauljp16 <raul_astra16@hotmail.com>
2025-04-18 18:33:53 +0200

    pagina construcción

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Initial_Info from "@/components/Initial_Info";
-import News from "@/components/News";
-import { getCars } from "./lib/Service";
-import Cars_list_preview from "@/components/Cars_list_preview";
-import Contact from "@/components/Contact";
+// import Initial_Info from "@/components/Initial_Info";
+// import News from "@/components/News";
+// import { getCars } from "./lib/Service";
+// import Cars_list_preview from "@/components/Cars_list_preview";
+// import Contact from "@/components/Contact";
 
 export default async function Home() {
   // const cars = await getCars();
